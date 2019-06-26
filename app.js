e45ebb8df8422909666e97c47bd72859c3547118
Jsanchez <jsanchez@fedefarma.com>
2019-06-26 10:05:11 +0200

    Cambios iniciales

---
+++ app.js
@@ -0,0 +1,33 @@
+const lugar = require('./lugar/lugar');
+const clima = require('./clima/clima');
+
+// Cargar modulos y crear nueva aplicacion
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // soporte para bodies codificados en jsonsupport
+app.use(bodyParser.urlencoded({ extended: true })); // soporte para bodies codificados
+
+const argv = require('yargs').options({
+    direccion: {
+        alias: 'd',
+        desc: 'Dirección de la ciudad para obtener el clima',
+        demand: true
+    }
+}).argv;
+
+let getInfo = async(direccion) => {
+    try {
+        console.log('Dato introducido: ' + direccion);
+        var coors = await lugar.getLugarLatLng(direccion);
+        let temp = await clima.getClima(coors.lat, coors.lng);
+
+        return (`El clima en ${coors.direccion} es de ${temp}`);
+    } catch (e) {
+        return (`No se puede determinar el clima para ${direccion}`);
+    }
+}
+
+getInfo(argv.direccion)
+    .then(mensaje => console.log(mensaje))
+    .catch(e => console.log(e));
