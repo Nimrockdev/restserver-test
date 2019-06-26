6d0f77707972fff5b0f5b5af34c2a7b05fb56142
Javier <jsg8405@gmail.com>
2019-06-26 15:54:56 +0200

    Add new services

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 const lugar = require('./lugar/lugar');
 const clima = require('./clima/clima');
-
-// Cargar modulos y crear nueva aplicacion
+const date = require('./date/date');
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+
+
 app.use(bodyParser.json()); // soporte para bodies codificados en jsonsupport
 app.use(bodyParser.urlencoded({ extended: true })); // soporte para bodies codificados
 
-// const argv = require('yargs').options({
-//     direccion: {
-//         alias: 'd',
-//         desc: 'Dirección de la ciudad para obtener el clima',
-//         demand: true
-//     }
-// }).argv;
 
-// getInfo(argv.direccion)
-//     .then(mensaje => console.log(mensaje))
-//     .catch(e => console.log(e));
+let salarios = [
+    { id: 1, salario: 1000 },
+    { id: 2, salario: 2000 }
+];
+
+
 
 let getInfo = async(direccion) => {
     try {
@@ -32,10 +29,54 @@ let getInfo = async(direccion) => {
     }
 }
 
-getInfo(argv.direccion)
-    .then(mensaje => console.log(mensaje))
-    .catch(e => console.log(e));
+// getInfo(argv.direccion)
+//     .then(mensaje => console.log(mensaje))
+//     .catch(e => console.log(e));
 
-getInfo(argv.direccion)
-    .then(mensaje => console.log(mensaje))
-    .catch(e => console.log(e));
+
+app.listen(3000, () => {
+    console.log('Listening port: ', 3000);
+    date.initServiceData();
+});
+
+app.get('/hw', function(req, res) {
+    res.json('Hello World, app developed in node!');
+});
+
+app.get('/time', function(req, res) {
+    res.json(date.getTime());
+});
+
+app.get('/salarios', function(req, res) {
+    res.json(salarios);
+});
+
+app.get('/getTimeServiceData', function(req, res) {
+
+    res.json(date.getTimeServiceData());
+});
+
+
+
+
+
+
+
+
+
+
+/*Deprecated*/
+// getInfo(argv.direccion)
+//     .then(mensaje => console.log(mensaje))
+//     .catch(e => console.log(e));
+// const argv = require('yargs').options({
+//     direccion: {
+//         alias: 'd',
+//         desc: 'Dirección de la ciudad para obtener el clima',
+//         demand: true
+//     }
+// }).argv;
+
+// getInfo(argv.direccion)
+//     .then(mensaje => console.log(mensaje))
+//     .catch(e => console.log(e));
