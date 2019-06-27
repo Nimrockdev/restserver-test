17131518e998e1d5936034e81fd440d28799381d
Javier <jsg8405@gmail.com>
2019-06-27 10:17:14 +0200

    add persistent for data static

---
+++ data/persistent.js
@@ -0,0 +1,16 @@
+let salarios = [
+    { id: 1, salario: 1000 },
+    { id: 2, salario: 2000 },
+    { id: 3, salario: 3000 },
+    { id: 4, salario: 4000 },
+    { id: 5, salario: 5000 },
+    { id: 6, salario: 6000 },
+    { id: 7, salario: 7000 },
+    { id: 8, salario: 8000 },
+    { id: 9, salario: 9000 },
+    { id: 10, salario: 10000 }
+];
+
+module.exports = {
+    salarios
+}
