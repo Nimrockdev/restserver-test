bb64851f536841ef045eb82e9f0042def06c866e
Javier <jsg8405@gmail.com>
2019-06-27 10:18:55 +0200

    delete array salarios

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const lugar = require('./lugar/lugar');
 const clima = require('./clima/clima');
 const date = require('./date/date');
+const persistent = require('./data/persistent');
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
@@ -8,12 +9,6 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.json()); // soporte para bodies codificados en jsonsupport
 app.use(bodyParser.urlencoded({ extended: true })); // soporte para bodies codificados
-
-
-let salarios = [
-    { id: 1, salario: 1000 },
-    { id: 2, salario: 2000 }
-];
 
 let direccion = { dir: '', lat: 0, lng: 0 };
 
@@ -29,6 +24,7 @@ let getInfo = async(direccion) => {
     }
 }
 
+/*call old*/
 // getInfo(argv.direccion)
 //     .then(mensaje => console.log(mensaje))
 //     .catch(e => console.log(e));
@@ -48,15 +44,11 @@ app.get('/time', function(req, res) {
 });
 
 app.get('/salarios', function(req, res) {
-    res.json(salarios);
+    res.json(persistent.salarios);
 });
 
 app.get('/getTimeServiceData', function(req, res) {
     res.json(date.getTimeServiceData());
-});
-
-app.get('/weather', function(req, res) {
-    res.json('weather');
 });
 
 app.get('/location', function(req, res) {
@@ -65,7 +57,6 @@ app.get('/location', function(req, res) {
     if (location != 'error') {
 
         lugar.getLugar(location).then(mensaje => {
-            // console.log(mensaje);
             res.json(mensaje);
         }).catch(e => {
             console.log(Error, e);
@@ -74,6 +65,43 @@ app.get('/location', function(req, res) {
 
     } else {
         respuesta = 'The specified location is erroneous';
+    }
+
+});
+
+app.get('/weather', function(req, res) {
+
+    let location = req.query.location || 'error';
+
+    if (location != 'error') {
+
+        /*old call*/
+        // getInfo(location)
+        //     .then(mensaje => console.log(mensaje))
+        //     .catch(e => console.log(e));
+
+        lugar.getLugar(location).then(mensaje => {
+
+            var lat = mensaje.lat; //Guardamos las variables en local,desde mensaje, devuelve undefined en lng
+            var lng = mensaje.lng;
+            var loc = mensaje.direccion;
+
+            clima.getClima(lat, lng).then(mensaje => {
+                res.send(`La temperatura en ${loc} es de ${mensaje} ºC`)
+            }).catch(e => {
+                console.log(Error, e);
+                res.json(e);
+            })
+
+        }).catch(e => {
+            console.log(Error, e);
+            res.json(e);
+        })
+
+    } else {
+        var respuesta = 'The specified location is erroneous';
+        console.log(respuesta);
+        res.send(respuesta);
     }
 
 });
