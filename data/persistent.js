ec983dfddef73b75db8a6f90e4acdc712136d0c0
Javier <jsg8405@gmail.com>
2019-07-10 10:15:09 +0200

    add persistent products from test local

--- data/persistent.js
+++ data/persistent.js
@@ -1,3 +1,5 @@
+/*only test local*/
+
 let salarios = [
     { id: 1, salario: 1000 },
     { id: 2, salario: 2000 },
@@ -11,6 +13,36 @@ let salarios = [
     { id: 10, salario: 10000 }
 ];
 
+let products = {
+    "ok": true,
+    "productos": [{
+            "disponible": true,
+            "_id": "5c46a71bc0d13400175517bf",
+            "nombre": "Cafe",
+            "precioUni": 5.1,
+            "categoria": null,
+            "__v": 0
+        },
+        {
+            "disponible": true,
+            "_id": "5c46a72dc0d13400175517c0",
+            "nombre": "Leche",
+            "precioUni": 5.1,
+            "categoria": null,
+            "__v": 0
+        },
+        {
+            "disponible": true,
+            "_id": "5ced394e6d9c26001782b1b9",
+            "nombre": "Leche 5",
+            "precioUni": 5.9,
+            "categoria": null,
+            "__v": 0
+        }
+    ]
+};
+
 module.exports = {
-    salarios
+    salarios,
+    products
 }
